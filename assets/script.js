f72ea2399a1518d5967f914ee65039edf3a3a138
artbat6 <arthurbatesdev@gmail.com>
2021-03-27 16:16:42 -0500

    added humidity, uv, and wind to five day

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,8 @@ function getCity() {
   getTodayWeatherData();
 }
 
+//--------------------------------------------------------------------------
+
 function getTodayWeatherData() {
   fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -22,68 +24,119 @@ function getTodayWeatherData() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data.main.temp);
+      // console.log(data.main.temp);
+      // console.log(data);
       //use query selector to get ID of where the temp data will be displayed
       var h1tempEl = document.querySelector("#h1temp");
       h1tempEl.innerText = data.main.temp + "º";
+      console.log(data);
+      var iconCodeToday = data.weather[0].icon;
+      var imgElToday = document.querySelector("#iconToday");
+      imgElToday.src =
+        "http://openweathermap.org/img/w/" + iconCodeToday + ".png";
       populateCityName(data);
 
+      var humidityTodayEl = document.getElementById("humidityToday");
+
+      humidityTodayEl.textContent = "Humidity: " + data.main.humidity + "%";
+
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      console.log(lat);
+      console.log(lon);
+
       return fetch(
-        "https://api.openweathermap.org/data/2.5/forecast?q=" +
-          cityEl +
-          "&units=imperial&appid=" +
+        "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+          lat +
+          "&lon=" +
+          lon +
+          "&appid=" +
           apiKey
-      ) //convert response to JSON
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (response) {
-          var indicesForDaysINeed = [4, 12, 20, 28, 36];
-          for (var i = 0; i < 5; i++) {
-            var dayIndex = indicesForDaysINeed[i];
-            console.log("DAY " + i);
-            // get the date starting with tomorrow [1]
-            var futureDateEl = response.list[dayIndex].dt_txt;
-            // get temperature for the day
-            var futureTemp = response.list[dayIndex].main.temp;
-            console.log(futureDateEl);
-            console.log(futureTemp);
-            console.log(response);
-            // var tempFills = document.getElementsByClassName("temp");
-            // tempFills.innerText = dayIndex;
-            var dateLabel = document.querySelector("#date" + i);
-            var tempLabel = document.querySelector("#temp" + i);
-            tempLabel.innerText = futureTemp;
-            dateLabel.innerText = futureDateEl;
+      );
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log("The UV index is:" + response.value);
+      //
+      var todayUvEl = document.getElementById("uvIndexToday");
+      todayUvEl.textContent = "UV index: " + response.value;
 
-            var iconCode = response.list[dayIndex].weather[0].icon;
-            var imgEl = document.querySelector("#icon" + i);
-            imgEl.src = "http://openweathermap.org/img/w/" + iconCode + ".png";
-
-            console.log(iconCode);
-
-            // var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
-
-            // console.log iconCode;
-            // console.log(response.list[0].main.temp);
-            // console.log(response.list[0].dt_txt);
-            //var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
-            // console.log(response);
-
-            // I have futureDateEl, futureTemp and iconURL for day0, day1, day2, day3, day4.
-            // document.getElementById('date'+i) and set it to futureDateEl
-            // document.getElementById('temp'+i) and set it to futureTemp
-            // same with the icon
-
-            //use query selector to get ID of where the temp data will be displayed
-          }
-        });
+      if (response.value <= 2) {
+        $("span").attr("class", "btn btn-outline-success");
+      }
+      if (response.value > 2 && response.value <= 5) {
+        $("span").attr("class", "btn btn-outline-warning");
+      }
+      if (response.value > 5) {
+        $("span").attr("class", "btn btn-outline-danger");
+      }
     });
+  getFiveDayWeather();
 
-  function populateCityName(data) {
-    var cityHeadingEl = document.getElementById("currentCity");
-    cityHeadingEl.innerText = data.name;
+  //----------------------------------------------------------------------------
+
+  function getFiveDayWeather() {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        cityEl +
+        "&units=imperial&appid=" +
+        apiKey
+    ) //convert response to JSON
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        var indicesForDaysINeed = [0, 8, 16, 24, 32];
+        for (var i = 0; i < 5; i++) {
+          var dayIndex = indicesForDaysINeed[i];
+          // console.log("DAY " + i);
+          // get the date starting with tomorrow [1]
+          var futureDateEl = formatDate(response.list[dayIndex].dt_txt);
+          // get temperature for the day
+          var futureTemp = response.list[dayIndex].main.temp;
+          console.log(response);
+          var futureHumidity = response.list[dayIndex].main.humidity;
+          var futureWind = response.list[dayIndex].wind.speed;
+          console.log(futureDateEl);
+          console.log(futureTemp);
+          console.log(response);
+          var humidityLabel = document.querySelector("#humidity" + i);
+          humidityLabel.innerText = "Humidity: " + futureHumidity;
+          console.log("Humidity: " + futureHumidity);
+
+          var dateLabel = document.querySelector("#date" + i);
+          dateLabel.innerText = futureDateEl;
+
+          var tempLabel = document.querySelector("#temp" + i);
+          tempLabel.innerText = futureTemp + "º";
+
+          var windFiveEl = document.querySelector("#wind" + i)
+          windFiveEl.textContent = "Wind:" + futureWind + "mph";
+
+          var iconCode = response.list[dayIndex].weather[0].icon;
+          var imgEl = document.querySelector("#icon" + i);
+          imgEl.src = "http://openweathermap.org/img/w/" + iconCode + ".png";
+          // console.log(iconCode)
+        }
+      });
   }
+}
 
-  // function populateFiveDay() {
+function formatDate(futureDateEl) {
+  // console.log(dateString);
+  var formattedDate = "";
+  // split on empty space
+  var chop1 = futureDateEl.split(" ");
+  // console.log(chop1);
+  chop2 = chop1[0].split("-");
+  formattedDate = chop2[1] + "/" + chop2[2];
+  return formattedDate;
+  console.log(formattedDate); // return formattedDate;
+}
+
+function populateCityName(data) {
+  var cityHeadingEl = document.getElementById("currentCity");
+  cityHeadingEl.innerText = data.name;
 }
